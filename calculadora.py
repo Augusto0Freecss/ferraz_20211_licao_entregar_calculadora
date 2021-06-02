7ba7953e04532cc935db1ae650056b8dbc302916
Augusto0Freecss <luc2026@gmail.com>
2021-06-01 22:00:10 -0300

    Multiplicação por função

--- calculadora.py
+++ calculadora.py
@@ -2,34 +2,29 @@ def potencia (a, b):
   pass
 def divisao (a, b):
   pass
+
 def multiplicacao (a, b):
   pass
+  return a * b
 
 def subtracao (a, b):
-  pass
   return a - b
-
 def soma (a, b):
   return a + b
 a = int(input("Digite o primeiro valor"))
 b = int(input ("Digite o segundo valor"))
-
 operacao = input("+: Soma\n-: Subtração\n*: Multiplicação\n/: Divisão\n**: Exponenciação")
-
 if operacao == '+':
   resultado = soma(a, b)
-
 elif operacao == '-':
-  resultado = a - b
   resultado = subtracao(a, b)
 
 elif operacao == '*':
   resultado = a * b
+  resultado = multiplicacao(a, b)
 
 elif operacao == '/':
   resultado = a // b
-
 else:
   resultado = a ** b
-
-print (resultado) 
+print (resultado)
